86eb53e6daf74b7b16f517038341beeb80055a4b
Roberto Covarelli <roberto.covarelli@cern.ch>
2024-01-31 14:03:41 +0100

    tutorial files

--- bin/Powheg/Templates/prepareCondorScript_template.sh
+++ bin/Powheg/Templates/prepareCondorScript_template.sh
@@ -4,6 +4,7 @@ error                   = ${logname}_$$(ProcId).err
 log                     = ${logname}.log
 initialdir              = ${rootfolder}/${folderName}
 +JobFlavour             = "${queue}"
+requirements            = (OpSysAndVer =?= "CentOS7")
 periodic_remove         = JobStatus == 5  
 WhenToTransferOutput    = ON_EXIT_OR_EVICT 
 																
